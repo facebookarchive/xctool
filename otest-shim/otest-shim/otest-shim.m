b081ed900de59b504002ebd1a676de102b0b37b9
Fred Potter <fpotter@fb.com>
2013-04-24 12:20:53 -0700

    adding LICENSE + copyright/license headers.
    Summary: ^^^
    
    Test Plan: None

--- otest-shim/otest-shim/otest-shim.m
+++ otest-shim/otest-shim/otest-shim.m
@@ -1,3 +1,18 @@
+//
+// Copyright 2013 Facebook
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
 #import <objc/message.h>
 #import <objc/runtime.h>
@@ -33,7 +48,7 @@ static void SwizzleClassSelectorForFunction(Class cls, SEL sel, IMP newImp)
   method_exchangeImplementations(originalMethod, replacedMethod);
 }
 
-void PrintJSON(id JSONObject)
+static void PrintJSON(id JSONObject)
 {
   NSError *error = nil;
   NSData *data = [NSJSONSerialization dataWithJSONObject:JSONObject options:0 error:&error];
