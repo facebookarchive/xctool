11d559fc5bff3c532e2b596838e37082b82eac29
Ben Gertzfield <beng@fb.com>
2013-04-25 13:53:21 -0700

    Print pretty output when rebuilding xctool
    Summary:
    Does what it says on the tin. This depends on D789258.
    
    Screenshot: https://www.facebook.com/pxlcld/kf8S
    
    Test Plan: Tools/xctool.sh made the above screenshot. ^^^

--- xctool.sh
+++ xctool.sh
@@ -3,17 +3,43 @@
 # Compile xctool on-demand so we don't have to check-in binaries.
 #
 
+set -e
+
 XCTOOL_DIR=$(cd $(dirname $0); pwd)
 
 TEMP_PATH=$(/usr/bin/mktemp -t xctool-build)
 trap "rm -f $TEMP_PATH" EXIT
 
-"$XCTOOL_DIR"/build.sh > $TEMP_PATH 2>&1
+BUILD_NEEDED_TOOL_PATH="$XCTOOL_DIR"/build_needed.sh
+BUILD_NEEDED=$($BUILD_NEEDED_TOOL_PATH $*)
 
-if [[ $? -ne 0 ]]; then
-  echo "ERROR: Failed to build xctool:"
-  cat $TEMP_PATH
-  exit 1
+COLOR_BRIGHT_WHITE="\033[1;97m"
+COLOR_BOLD_RED="\033[1;31m"
+COLOR_GREEN="\033[0;32m"
+COLOR_NORMAL="\033[0m"
+CHECK_MARK="\xe2\x9c\x93"
+
+if [ "$BUILD_NEEDED" -eq 1 ]; then
+  echo -e "${COLOR_BRIGHT_WHITE}=== BUILDING XCTOOL ===${COLOR_NORMAL}"
+  echo
+  echo -e "  $XCTOOL_DIR/build.sh"
+
+  # Alas, date on Mac OS X has no %N, so we can't get milliseconds here.
+  BUILD_TIME_START=$SECONDS
+  if ! "$XCTOOL_DIR"/build.sh > $TEMP_PATH 2>&1; then
+    echo
+    echo -e "${COLOR_BOLD_RED}ERROR${COLOR_NORMAL}: Failed to build xctool:"
+    cat $TEMP_PATH
+    exit 1
+  fi
+
+  BUILD_TIME_END=$SECONDS
+
+  BUILD_DURATION_MS=$(((${BUILD_TIME_END}-${BUILD_TIME_START}) * 1000))
+
+  echo -ne "      ${COLOR_GREEN}${CHECK_MARK}${COLOR_NORMAL} Built xctool "
+  echo -e "${COLOR_GREEN}(${BUILD_DURATION_MS} ms)${COLOR_NORMAL}"
+  echo
 fi
 
 # Will be a short git hash or just '.' if we're not in a git repo.
