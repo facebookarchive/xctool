6c43af5bc862fd6bf3bca93c9e0b371ef21e03cd
Matt Galloway <matt@galloway.me.uk>
2013-05-03 11:49:00 +0100

    Add an archive action

--- xctool/xctool/Options.m
+++ xctool/xctool/Options.m
@@ -16,6 +16,7 @@
 
 #import "Options.h"
 
+#import "ArchiveAction.h"
 #import "BuildAction.h"
 #import "BuildTestsAction.h"
 #import "CleanAction.h"
@@ -35,6 +36,7 @@
            [BuildTestsAction class],
            [RunTestsAction class],
            [TestAction class],
+           [ArchiveAction class],
            ];
 }
 
