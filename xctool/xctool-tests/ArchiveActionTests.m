fa81b343c27be65305ea271ca37c97e321a8f1d2
Fred Potter <fpotter@fb.com>
2013-06-01 13:10:33 -0700

    a simple test for the 'archive' action.
    Just make sure xcodebuild gets invoked with 'archive'

---
+++ xctool/xctool-tests/ArchiveActionTests.m
@@ -0,0 +1,45 @@
+
+#import <SenTestingKit/SenTestingKit.h>
+
+#import "FakeTask.h"
+#import "FakeTaskManager.h"
+#import "LaunchHandlers.h"
+#import "TestUtil.h"
+#import "XCTool.h"
+
+@interface ArchiveActionTests : SenTestCase
+@end
+
+@implementation ArchiveActionTests
+
+- (void)testArchiveActionTriggersBuildForProjectAndScheme
+{
+  [[FakeTaskManager sharedManager] runBlockWithFakeTasks:^{
+    [[FakeTaskManager sharedManager] addLaunchHandlerBlocks:@[
+     // Make sure -showBuildSettings returns some data
+     [LaunchHandlers handlerForShowBuildSettingsWithProject:TEST_DATA @"TestProject-Library/TestProject-Library.xcodeproj"
+                                                     scheme:@"TestProject-Library"
+                                               settingsPath:TEST_DATA @"TestProject-Library-showBuildSettings.txt"],
+     ]];
+
+    XCTool *tool = [[[XCTool alloc] init] autorelease];
+
+    tool.arguments = @[@"-project", TEST_DATA @"TestProject-Library/TestProject-Library.xcodeproj",
+                       @"-scheme", @"TestProject-Library",
+                       @"archive",
+                       ];
+
+    [TestUtil runWithFakeStreams:tool];
+
+    assertThat([[[FakeTaskManager sharedManager] launchedTasks][0] arguments],
+               equalTo(@[
+                       @"-project", TEST_DATA @"TestProject-Library/TestProject-Library.xcodeproj",
+                       @"-scheme", @"TestProject-Library",
+                       @"-configuration", @"Debug",
+                       @"-sdk", @"iphonesimulator6.0",
+                       @"archive",
+                       ]));
+  }];
+}
+
+@end
