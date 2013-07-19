04ab34ccb914f53d125e619e67a4e35f6ad82bd4
Katie Coons <kcoons@fb.com>
2013-07-19 14:35:27 -0700

    Add source context to exceptions and analyzer warnings

---
+++ xctool/xctool-tests/TestData/ContextTest.m
@@ -0,0 +1,20 @@
+//
+//  ContextTest.m
+//
+//  Created on 7/22/13.
+//
+//
+
+#import "ContextTest.h"
+
+@implementation ContextTest
+
+static int test() {
+  NSObject *blah = [[NSObject alloc] init];
+}
+
+static int test2() {
+  NSObject *blah = [[NSObject alloc] init];        
+}
+
+@end
