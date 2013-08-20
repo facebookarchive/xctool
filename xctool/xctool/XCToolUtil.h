b0bc17d6598fad04b5e863a719c499242fe24eb0
Fred Potter <fpotter@fb.com>
2013-08-20 13:25:38 -0700

    GetAvailableSDKsAndAliases() should empty the env before running xcodebuild.
    Previoiusly, we never called `setEnvironment:` on the NSTask, and so the
    caller's environment would pass through.
    
    Usually that's OK, but when running tests from Xcode,
    OBJC_DISABLE_GC=YES will get set and then `xcodebuild` will fail to run
    (because it uses GC).
    
    Note that DEVELOPER_DIR can still be used to be override which
    `xcodebuild` to use, since `XcodeDeveloperDirPath()` will call out to
    `xcodebuild`.

--- xctool/xctool/XCToolUtil.h
+++ xctool/xctool/XCToolUtil.h
@@ -87,3 +87,8 @@ NSArray *AvailableReporters();
  Uses realpath() to resolve an relative path.
  */
 NSString *AbsolutePathFromRelative(NSString *path);
+
+/**
+ * Returns the contents of `/etc/paths` in the form of `path1:path2:path3`.
+ */
+NSString *SystemPaths();
