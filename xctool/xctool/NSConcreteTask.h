19e25ccac08e32503ad2727a107acfc249928b28
Fred Potter <fpotter@fb.com>
2013-09-12 22:36:46 -0700

    force i386 when running `otest` for iOS logic tests.
    As of the Xcode 5 GM, `otest` for the iPhoneSimulator SDK became
    a universal binary.
    
    The problem with that is that 1) Xcode doesn't yet build iPhoneSimulator
    binaries as x86_64 - they're all still i386; and 2) otest-shim + friends
    are still i386 only.
    
    For now, we can just force i386 and things will keep working.  At
    somepoint Apple is going to start building simulator binaries as x86_64
    and we'll have to make otest-shim + otest-query universal.
    
    We'll probably also have to do what Xcode does when testing universal
    binaries - i.e. runs tests twice, once as i386 and once as x86_64.

---
+++ xctool/xctool/NSConcreteTask.h
@@ -0,0 +1,38 @@
+//
+// Copyright 2013 Facebook
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+/**
+ * NSConcreteTask is the true implementation behind NSTask.
+ */
+@interface NSConcreteTask : NSTask
+
+/**
+ * Internal dictionary of settings.  Passed to `launchWithDictionary:`.
+ */
+- (NSMutableDictionary *)taskDictionary;
+
+/**
+ * Set to `@[@(CPU_TYPE_X86_64)]` to prefer x86_64, or to `@[@(CPU_TYPE_I386)]`
+ * to prefer i386.
+ */
+- (void)setPreferredArchitectures:(NSArray *)architectures;
+
+/**
+ * Returns preferred architectures.
+ */
+- (id)preferredArchitectures;
+
+@end
