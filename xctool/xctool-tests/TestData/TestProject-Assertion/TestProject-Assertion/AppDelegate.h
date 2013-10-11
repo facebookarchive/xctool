0cfd62df997420a680bf203c1ef728745c4c3adb
Ryan Rhee <ryan.y.rhee@gmail.com>
2013-10-11 16:20:59 -0700

    Adding test project for assertions

---
+++ xctool/xctool-tests/TestData/TestProject-Assertion/TestProject-Assertion/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestProject-Exception
+//
+//  Created by Ryan Rhee on 10/9/13.
+//  Copyright (c) 2013 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
