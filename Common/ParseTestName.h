b178567088105c9572f0f00be317aa861b234c7f
Fred Potter <fpotter@fb.com>
2013-10-17 22:49:30 -0700

    move `ParseClassAndMethodFromTestName` into the Common dir
    This function is going to be used by otest-query as well, so lets move
    it to the common dir.

---
+++ Common/ParseTestName.h
@@ -0,0 +1,31 @@
+//
+// Copyright 2013 Facebook
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+/**
+ This file is shared by otest-query and otest-shim.  If we start sharing more
+ functions between these projects, we should rename this file to a more generic
+ name and put those here.
+ */
+
+/**
+ Parses a test name like '-[TestClass someMethod]' into its class and method
+ name parts.
+ 
+ @param className Out param where class name will be stored.
+ @param methodName Out param where method name will be stored.
+ @param testName Full name of test, e.g. '-[TestClass someMethod]'
+ */
+void ParseClassAndMethodFromTestName(NSString **className, NSString **methodName, NSString *testName);
