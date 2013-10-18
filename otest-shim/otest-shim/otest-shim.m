46c30e14346b32c679c080fc204d66a0e37f3241
Fred Potter <fpotter@fb.com>
2013-10-17 22:49:30 -0700

    move `ParseClassAndMethodFromTestName` into the Common dir
    This function is going to be used by otest-query as well, so lets move
    it to the common dir.

--- otest-shim/otest-shim/otest-shim.m
+++ otest-shim/otest-shim/otest-shim.m
@@ -24,6 +24,7 @@
 #import <SenTestingKit/SenTestingKit.h>
 #import "XCTest.h"
 
+#import "ParseTestName.h"
 #import "ReporterEvents.h"
 #import "Swizzle.h"
 
@@ -84,27 +85,6 @@ static NSString *StripAnsi(NSString *inputString)
                                                              range:NSMakeRange(0, [inputString length])
                                                       withTemplate:@""];
   return outputString;
-}
-
-void ParseClassAndMethodFromTestName(NSString **className, NSString **methodName, NSString *testName)
-{
-  NSCAssert(className, @"className should be non-nil");
-  NSCAssert(methodName, @"methodName should be non-nil");
-  NSCAssert(testName, @"testName should be non-nil");
-
-  NSRegularExpression *testNameRegex =
-    [NSRegularExpression regularExpressionWithPattern:@"^-\\[(\\w+) (\\w+)\\]$"
-                                              options:0
-                                                error:nil];
-  NSTextCheckingResult *match =
-    [testNameRegex firstMatchInString:testName
-                              options:0
-                                range:NSMakeRange(0, [testName length])];
-  NSCAssert(match && [match numberOfRanges] == 3,
-            @"Test name seems to be malformed: %@", testName);
-
-  *className = [testName substringWithRange:[match rangeAtIndex:1]];
-  *methodName = [testName substringWithRange:[match rangeAtIndex:2]];
 }
 
 static void PrintJSON(id JSONObject)
