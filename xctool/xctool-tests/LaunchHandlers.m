facebookarchive/xctool

commit 2a7abf856dcc46e5e40086d5f5413dac8598c4dd

Use ProductVersion when constructing CFFIXED_USER_HOME
[#227]

xctool/xctool/OCUnitIOSTestQueryRunner.m
@@ -39,7 +39,8 @@ NSString *SimulatorSDKRootPathWithVersion(NSString *version)
 - (NSDictionary *)envForQueryInIOSBundleWithAdditionalEnv:(NSDictionary *)additionalEnv
 {
   NSString *version = [[self sdk] stringByReplacingOccurrencesOfString:@"iphonesimulator" withString:@""];
-  NSString *simulatorHome = [NSString stringWithFormat:@"%@/Library/Application Support/iPhone Simulator/%@", NSHomeDirectory(), version];
+  NSString *productVersion = GetProductVersion(version);
+  NSString *simulatorHome = [NSString stringWithFormat:@"%@/Library/Application Support/iPhone Simulator/%@", NSHomeDirectory(), productVersion];
   NSString *sdkRootPath = SimulatorSDKRootPathWithVersion(version);
 
   NSMutableDictionary *env = [NSMutableDictionary dictionaryWithDictionary:@{

commit 4308ffd6565acea7e590a746339eb8d827d3feee

otest-query-ios now launches via iPhoneSimulator.platform/usr/bin/sim
This fixes the bug that started in 2a7abf856 where otest-query would
fail with --

```
failed: Failed to query the list of test cases in the test bundle: Error while parsing JSON: The data has been corrupted.:
```

The problem turns out to be how we set CFFIXED_USER_HOME - I don't quite
understand why, but I've narrowed it down to that.

In that commmit, we began setting CFFIXED_USER_HOME

sim-shim/sim-shim.xcconfig
@@ -14,13 +14,14 @@
 // limitations under the License.
 //
 
-#import "OCUnitTestQueryRunner.h"
+#include "../Configurations/Project.xcconfig"
 
-NSString *SimulatorSDKRootPathWithVersion(NSString *version);
+#include "../Configurations/Install.xcconfig"
+XT_INSTALL_DIR = lib
 
-@interface OCUnitIOSTestQueryRunner : OCUnitTestQueryRunner
+MACOSX_DEPLOYMENT_TARGET = 10.7
 
-- (NSDictionary *)envForQueryInIOSBundleWithAdditionalEnv:(NSDictionary *)additionalEnv;
-- (NSString *)sdk;
-
-@end
+// In Xcode 4, .../iPhoneSimulator.platform/usr/bin/sim is only 32-bit.
+VALID_ARCHS_0400 = i386
+VALID_ARCHS_0500 = i386 x86_64
+VALID_ARCHS = $(VALID_ARCHS_$(XCODE_VERSION_MAJOR))

sim-shim/sim-shim.xcodeproj/project.pbxproj
@@ -1,0 +1,289 @@
+// !$*UTF8*$!
+{
+	archiveVersion = 1;
+	classes = {
+	};
+	objectVersion = 46;
+	objects = {
+
+/* Begin PBXBuildFile section */
+		28AF3217182716E20051F658 /* Cocoa.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 28AF3216182716E20051F658 /* Cocoa.framework */; };
+		28AF3221182716E20051F658 /* sim_shim.m in Sources */ = {isa = PBXBuildFile; fileRef = 28AF3220182716E20051F658 /* sim_shim.m */; };
+		28D9C5A0182725DC0032FEA8 /* Swizzle.h in Headers */ = {isa = PBXBuildFile; fileRef = 28D9C58E182725DC0032FEA8 /* Swizzle.h */; };
+		28D9C5A1182725DC0032FEA8 /* Swizzle.m in Sources */ = {isa = PBXBuildFile; fileRef = 28D9C58F182725DC0032FEA8 /* Swizzle.m */; };
+/* End PBXBuildFile section */
+
+/* Begin PBXFileReference section */
+		28AF3213182716E20051F658 /* sim-shim.dylib */ = {isa = PBXFileReference; explicitFileType = "compiled.mach-o.dylib"; includeInIndex = 0; path = "sim-shim.dylib"; sourceTree = BUILT_PRODUCTS_DIR; };
+		28AF3216182716E20051F658 /* Cocoa.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = Cocoa.framework; path = System/Library/Frameworks/Cocoa.framework; sourceTree = SDKROOT; };
+		28AF3219182716E20051F658 /* Foundation.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = Foundation.framework; path = System/Library/Frameworks/Foundation.framework; sourceTree = SDKROOT; };
+		28AF321A182716E20051F658 /* CoreData.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = CoreData.framework; path = System/Library/Frameworks/CoreData.framework; sourceTree = SDKROOT; };
+		28AF321B182716E20051F658 /* AppKit.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = AppKit.framework; path = System/Library/Frameworks/AppKit.framework; sourceTree = SDKROOT; };
+		28AF321E182716E20051F658 /* sim-shim-Prefix.pch */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = "sim-shim-Prefix.pch"; sourceTree = "<group>"; };
+		28AF3220182716E20051F658 /* sim_shim.m */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.objc; path = sim_shim.m; sourceTree = "<group>"; };
+		28AF3227182716E20051F658 /* XCTest.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = XCTest.framework; path = Library/Frameworks/XCTest.framework; sourceTree = DEVELOPER_DIR; };
+		28D9C57F182725030032FEA8 /* sim-shim.xcconfig */ = {isa = PBXFileReference; lastKnownFileType = text.xcconfig; path = "sim-shim.xcconfig"; sourceTree = "<group>"; };
+		28D9C58E182725DC0032FEA8 /* Swizzle.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = Swizzle.h; sourceTree = "<group>"; };
+		28D9C58F182725DC0032FEA8 /* Swizzle.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = Swizzle.m; sourceTree = "<group>"; };
+/* End PBXFileReference section */
+
+/* Begin PBXFrameworksBuildPhase section */
+		28AF3210182716E20051F658 /* Frameworks */ = {
+			isa = PBXFrameworksBuildPhase;
+			buildActionMask = 2147483647;
+			files = (
+				28AF3217182716E20051F658 /* Cocoa.framework in Frameworks */,
+			);
+			runOnlyForDeploymentPostprocessing = 0;
+		};
+/* End PBXFrameworksBuildPhase section */
+
+/* Begin PBXGroup section */
+		28AF320A182716E20051F658 = {
+			isa = PBXGroup;
+			children = (
+				28D9C57F182725030032FEA8 /* sim-shim.xcconfig */,
+				28D9C580182725DC0032FEA8 /* Common */,
+				28AF321C182716E20051F658 /* sim-shim */,
+				28AF3215182716E20051F658 /* Frameworks */,
+				28AF3214182716E20051F658 /* Products */,
+			);
+			sourceTree = "<group>";
+		};
+		28AF3214182716E20051F658 /* Products */ = {
+			isa = PBXGroup;
+			children = (
+				28AF3213182716E20051F658 /* sim-shim.dylib */,
+			);
+			name = Products;
+			sourceTree = "<group>";
+		};
+		28AF3215182716E20051F658 /* Frameworks */ = {
+			isa = PBXGroup;
+			children = (
+				28AF3216182716E20051F658 /* Cocoa.framework */,
+				28AF3227182716E20051F658 /* XCTest.framework */,
+				28AF3218182716E20051F658 /* Other Frameworks */,
+			);
+			name = Frameworks;
+			sourceTree = "<group>";
+		};
+		28AF3218182716E20051F658 /* Other Frameworks */ = {
+			isa = PBXGroup;
+			children = (
+				28AF3219182716E20051F658 /* Foundation.framework */,
+				28AF321A182716E20051F658 /* CoreData.framework */,
+				28AF321B182716E20051F658 /* AppKit.framework */,
+			);
+			name = "Other Frameworks";
+			sourceTree = "<group>";
+		};
+		28AF321C182716E20051F658 /* sim-shim */ = {
+			isa = PBXGroup;
+			children = (
+				28AF3220182716E20051F658 /* sim_shim.m */,
+				28AF321D182716E20051F658 /* Supporting Files */,
+			);
+			path = "sim-shim";
+			sourceTree = "<group>";
+		};
+		28AF321D182716E20051F658 /* Supporting Files */ = {
+			isa = PBXGroup;
+			children = (
+				28AF321E182716E20051F658 /* sim-shim-Prefix.pch */,
+			);
+			name = "Supporting Files";
+			sourceTree = "<group>";
+		};
+		28D9C580182725DC0032FEA8 /* Common */ = {
+			isa = PBXGroup;
+			children = (
+				28D9C58E182725DC0032FEA8 /* Swizzle.h */,
+				28D9C58F182725DC0032FEA8 /* Swizzle.m */,
+			);
+			name = Common;
+			path = ../Common;
+			sourceTree = "<group>";
+		};
+/* End PBXGroup section */
+
+/* Begin PBXHeadersBuildPhase section */
+		28AF3211182716E20051F658 /* Headers */ = {
+			isa = PBXHeadersBuildPhase;
+			buildActionMask = 2147483647;
+			files = (
+				28D9C5A0182725DC0032FEA8 /* Swizzle.h in Headers */,
+			);
+			runOnlyForDeploymentPostprocessing = 0;
+		};
+/* End PBXHeadersBuildPhase section */
+
+/* Begin PBXNativeTarget section */
+		28AF3212182716E20051F658 /* sim-shim */ = {
+			isa = PBXNativeTarget;
+			buildConfigurationList = 28AF3237182716E20051F658 /* Build configuration list for PBXNativeTarget "sim-shim" */;
+			buildPhases = (
+				28AF320F182716E20051F658 /* Sources */,
+				28AF3210182716E20051F658 /* Frameworks */,
+				28AF3211182716E20051F658 /* Headers */,
+			);
+			buildRules = (
+			);
+			dependencies = (
+			);
+			name = "sim-shim";
+			productName = "sim-shim";
+			productReference = 28AF3213182716E20051F658 /* sim-shim.dylib */;
+			productType = "com.apple.product-type.library.dynamic";
+		};
+/* End PBXNativeTarget section */
+
+/* Begin PBXProject section */
+		28AF320B182716E20051F658 /* Project object */ = {
+			isa = PBXProject;
+			attributes = {
+				LastUpgradeCheck = 0500;
+				ORGANIZATIONNAME = "Facebook, Inc.";
+			};
+			buildConfigurationList = 28AF320E182716E20051F658 /* Build configuration list for PBXProject "sim-shim" */;
+			compatibilityVersion = "Xcode 3.2";
+			developmentRegion = English;
+			hasScannedForEncodings = 0;
+			knownRegions = (
+				en,
+			);
+			mainGroup = 28AF320A182716E20051F658;
+			productRefGroup = 28AF3214182716E20051F658 /* Products */;
+			projectDirPath = "";
+			projectRoot = "";
+			targets = (
+				28AF3212182716E20051F658 /* sim-shim */,
+			);
+		};
+/* End PBXProject section */
+
+/* Begin PBXSourcesBuildPhase section */
+		28AF320F182716E20051F658 /* Sources */ = {
+			isa = PBXSourcesBuildPhase;
+			buildActionMask = 2147483647;
+			files = (
+				28D9C5A1182725DC0032FEA8 /* Swizzle.m in Sources */,
+				28AF3221182716E20051F658 /* sim_shim.m in Sources */,
+			);
+			runOnlyForDeploymentPostprocessing = 0;
+		};
+/* End PBXSourcesBuildPhase section */
+
+/* Begin XCBuildConfiguration section */
+		28AF3235182716E20051F658 /* Debug */ = {
+			isa = XCBuildConfiguration;
+			baseConfigurationReference = 28D9C57F182725030032FEA8 /* sim-shim.xcconfig */;
+			buildSettings = {
+				ALWAYS_SEARCH_USER_PATHS = NO;
+				CLANG_CXX_LANGUAGE_STANDARD = "gnu++0x";
+				CLANG_CXX_LIBRARY = "libc++";
+				CLANG_WARN_BOOL_CONVERSION = YES;
+				CLANG_WARN_CONSTANT_CONVERSION = YES;
+				CLANG_WARN_DIRECT_OBJC_ISA_USAGE = YES_ERROR;
+				CLANG_WARN_EMPTY_BODY = YES;
+				CLANG_WARN_ENUM_CONVERSION = YES;
+				CLANG_WARN_INT_CONVERSION = YES;
+				CLANG_WARN_OBJC_ROOT_CLASS = YES_ERROR;
+				CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
+				COPY_PHASE_STRIP = NO;
+				GCC_C_LANGUAGE_STANDARD = gnu99;
+				GCC_DYNAMIC_NO_PIC = NO;
+				GCC_ENABLE_OBJC_EXCEPTIONS = YES;
+				GCC_OPTIMIZATION_LEVEL = 0;
+				GCC_PREPROCESSOR_DEFINITIONS = (
+					"DEBUG=1",
+					"$(inherited)",
+				);
+				GCC_SYMBOLS_PRIVATE_EXTERN = NO;
+				GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
+				GCC_WARN_ABOUT_RETURN_TYPE = YES_ERROR;
+				GCC_WARN_UNINITIALIZED_AUTOS = YES;
+				GCC_WARN_UNUSED_FUNCTION = YES;
+				GCC_WARN_UNUSED_VARIABLE = YES;
+				SDKROOT = macosx;
+			};
+			name = Debug;
+		};
+		28AF3236182716E20051F658 /* Release */ = {
+			isa = XCBuildConfiguration;
+			baseConfigurationReference = 28D9C57F182725030032FEA8 /* sim-shim.xcconfig */;
+			buildSettings = {
+				ALWAYS_SEARCH_USER_PATHS = NO;
+				CLANG_CXX_LANGUAGE_STANDARD = "gnu++0x";
+				CLANG_CXX_LIBRARY = "libc++";
+				CLANG_WARN_BOOL_CONVERSION = YES;
+				CLANG_WARN_CONSTANT_CONVERSION = YES;
+				CLANG_WARN_DIRECT_OBJC_ISA_USAGE = YES_ERROR;
+				CLANG_WARN_EMPTY_BODY = YES;
+				CLANG_WARN_ENUM_CONVERSION = YES;
+				CLANG_WARN_INT_CONVERSION = YES;
+				CLANG_WARN_OBJC_ROOT_CLASS = YES_ERROR;
+				CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
+				COPY_PHASE_STRIP = YES;
+				DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";
+				ENABLE_NS_ASSERTIONS = NO;
+				GCC_C_LANGUAGE_STANDARD = gnu99;
+				GCC_ENABLE_OBJC_EXCEPTIONS = YES;
+				GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
+				GCC_WARN_ABOUT_RETURN_TYPE = YES_ERROR;
+				GCC_WARN_UNINITIALIZED_AUTOS = YES;
+				GCC_WARN_UNUSED_FUNCTION = YES;
+				GCC_WARN_UNUSED_VARIABLE = YES;
+				SDKROOT = macosx;
+			};
+			name = Release;
+		};
+		28AF3238182716E20051F658 /* Debug */ = {
+			isa = XCBuildConfiguration;
+			buildSettings = {
+				ARCHS = "$(ARCHS_STANDARD_32_64_BIT)";
+				DYLIB_COMPATIBILITY_VERSION = 1;
+				DYLIB_CURRENT_VERSION = 1;
+				GCC_PRECOMPILE_PREFIX_HEADER = YES;
+				GCC_PREFIX_HEADER = "sim-shim/sim-shim-Prefix.pch";
+				PRODUCT_NAME = "$(TARGET_NAME)";
+			};
+			name = Debug;
+		};
+		28AF3239182716E20051F658 /* Release */ = {
+			isa = XCBuildConfiguration;
+			buildSettings = {
+				ARCHS = "$(ARCHS_STANDARD_32_64_BIT)";
+				DYLIB_COMPATIBILITY_VERSION = 1;
+				DYLIB_CURRENT_VERSION = 1;
+				GCC_PRECOMPILE_PREFIX_HEADER = YES;
+				GCC_PREFIX_HEADER = "sim-shim/sim-shim-Prefix.pch";
+				PRODUCT_NAME = "$(TARGET_NAME)";
+			};
+			name = Release;
+		};
+/* End XCBuildConfiguration section */
+
+/* Begin XCConfigurationList section */
+		28AF320E182716E20051F658 /* Build configuration list for PBXProject "sim-shim" */ = {
+			isa = XCConfigurationList;
+			buildConfigurations = (
+				28AF3235182716E20051F658 /* Debug */,
+				28AF3236182716E20051F658 /* Release */,
+			);
+			defaultConfigurationIsVisible = 0;
+			defaultConfigurationName = Release;
+		};
+		28AF3237182716E20051F658 /* Build configuration list for PBXNativeTarget "sim-shim" */ = {
+			isa = XCConfigurationList;
+			buildConfigurations = (
+				28AF3238182716E20051F658 /* Debug */,
+				28AF3239182716E20051F658 /* Release */,
+			);
+			defaultConfigurationIsVisible = 0;
+			defaultConfigurationName = Release;
+		};
+/* End XCConfigurationList section */
+	};
+	rootObject = 28AF320B182716E20051F658 /* Project object */;
+}

sim-shim/sim-shim/sim-shim-Prefix.pch
@@ -1,0 +1,9 @@
+//
+//  Prefix header
+//
+//  The contents of this file are implicitly included at the beginning of every source file.
+//
+
+#ifdef __OBJC__
+  #import <Cocoa/Cocoa.h>
+#endif

sim-shim/sim-shim/sim_shim.m
@@ -1,0 +1,71 @@
+//
+// Copyright 2013 Facebook
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "Swizzle.h"
+
+@interface BootlegTask : NSObject
+{
+  BOOL _waitForDebugger;
+  BOOL _setExec;
+  int _cpuType;
+  NSString *_launchPath;
+  NSArray *_arguments;
+  NSDictionary *_environment;
+  NSDictionary *_sessionCookie;
+}
+
+@property int cpuType; // @synthesize cpuType=_cpuType;
+@property BOOL setExec; // @synthesize setExec=_setExec;
+@property BOOL waitForDebugger; // @synthesize waitForDebugger=_waitForDebugger;
+@property(copy) NSDictionary *sessionCookie; // @synthesize sessionCookie=_sessionCookie;
+@property(copy) NSDictionary *environment; // @synthesize environment=_environment;
+@property(copy) NSArray *arguments; // @synthesize arguments=_arguments;
+@property(copy) NSString *launchPath; // @synthesize launchPath=_launchPath;
+- (int)runUntilExit;
+
+@end
+
+static int BootlegTask_runUntilExit(BootlegTask *self, SEL sel)
+{
+  NSMutableDictionary *newEnv = [NSMutableDictionary dictionary];
+  [newEnv addEntriesFromDictionary:[self environment]];
+
+  [[[NSProcessInfo processInfo] environment] enumerateKeysAndObjectsUsingBlock:
+   ^(id key, id value, BOOL *stop){
+     if ([key hasPrefix:@"SIMSHIM_"]) {
+       NSString *newKey = [key stringByReplacingOccurrencesOfString:@"SIMSHIM_" withString:@""];
+       [newEnv setObject:value forKey:newKey];
+     }
+   }];
+
+  [self setValue:newEnv forKey:@"_environment"];
+
+  return (int)objc_msgSend(self, @selector(__BootlegTask_runUntilExit));
+}
+
+__attribute__((constructor)) static void Initializer()
+{
+  NSCAssert(NSClassFromString(@"BootlegTask") != nil, @"Class should exist.");
+
+  XTSwizzleSelectorForFunction(NSClassFromString(@"BootlegTask"),
+                               @selector(runUntilExit),
+                               (IMP)BootlegTask_runUntilExit);
+
+  // Don't cascade into other spawned processes.
+  unsetenv("DYLD_INSERT_LIBRARIES");
+}

xctool.xcworkspace/contents.xcworkspacedata
@@ -19,6 +19,9 @@
    <FileRef
       location = "group:otest-shim/otest-shim.xcodeproj">
    </FileRef>
+   <FileRef
+      location = "group:sim-shim/sim-shim.xcodeproj">
+   </FileRef>
    <Group
       location = "group:Common"
       name = "Common">

xctool/xctool.xcodeproj/project.pbxproj
@@ -107,8 +107,6 @@
 		AAC1E0B21811F5C3005A4FD5 /* OCUnitTestQueryRunner.m in Sources */ = {isa = PBXBuildFile; fileRef = AAC1E0B01811F5C3005A4FD5 /* OCUnitTestQueryRunner.m */; };
 		AAC1E0B51811F5E3005A4FD5 /* OCUnitOSXTestQueryRunner.m in Sources */ = {isa = PBXBuildFile; fileRef = AAC1E0B41811F5E3005A4FD5 /* OCUnitOSXTestQueryRunner.m */; };
 		AAC1E0B61811F5E3005A4FD5 /* OCUnitOSXTestQueryRunner.m in Sources */ = {isa = PBXBuildFile; fileRef = AAC1E0B41811F5E3005A4FD5 /* OCUnitOSXTestQueryRunner.m */; };
-		AAC1E0B91811F5F0005A4FD5 /* OCUnitIOSTestQueryRunner.m in Sources */ = {isa = PBXBuildFile; fileRef = AAC1E0B81811F5F0005A4FD5 /* OCUnitIOSTestQueryRunner.m */; };
-		AAC1E0BA1811F5F0005A4FD5 /* OCUnitIOSTestQueryRunner.m in Sources */ = {isa = PBXBuildFile; fileRef = AAC1E0B81811F5F0005A4FD5 /* OCUnitIOSTestQueryRunner.m */; };
 		AAC1E0BD18121071005A4FD5 /* OCUnitIOSLogicTestQueryRunner.m in Sources */ = {isa = PBXBuildFile; fileRef = AAC1E0BC18121071005A4FD5 /* OCUnitIOSLogicTestQueryRunner.m */; };
 		AAC1E0BE18121071005A4FD5 /* OCUnitIOSLogicTestQueryRunner.m in Sources */ = {isa = PBXBuildFile; fileRef = AAC1E0BC18121071005A4FD5 /* OCUnitIOSLogicTestQueryRunner.m */; };
 		AAC1E0C118121AC6005A4FD5 /* OCUnitIOSAppTestQueryRunner.m in Sources */ = {isa = PBXBuildFile; fileRef = AAC1E0C018121AC6005A4FD5 /* OCUnitIOSAppTestQueryRunner.m */; };
@@ -289,8 +287,6 @@
 		AAC1E0B01811F5C3005A4FD5 /* OCUnitTestQueryRunner.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = OCUnitTestQueryRunner.m; sourceTree = "<group>"; };
 		AAC1E0B31811F5E3005A4FD5 /* OCUnitOSXTestQueryRunner.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = OCUnitOSXTestQueryRunner.h; sourceTree = "<group>"; };
 		AAC1E0B41811F5E3005A4FD5 /* OCUnitOSXTestQueryRunner.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = OCUnitOSXTestQueryRunner.m; sourceTree = "<group>"; };
-		AAC1E0B71811F5F0005A4FD5 /* OCUnitIOSTestQueryRunner.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = OCUnitIOSTestQueryRunner.h; sourceTree = "<group>"; };
-		AAC1E0B81811F5F0005A4FD5 /* OCUnitIOSTestQueryRunner.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = OCUnitIOSTestQueryRunner.m; sourceTree = "<group>"; };
 		AAC1E0BB18121071005A4FD5 /* OCUnitIOSLogicTestQueryRunner.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = OCUnitIOSLogicTestQueryRunner.h; sourceTree = "<group>"; };
 		AAC1E0BC18121071005A4FD5 /* OCUnitIOSLogicTestQueryRunner.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = OCUnitIOSLogicTestQueryRunner.m; sourceTree = "<group>"; };
 		AAC1E0BF18121AC6005A4FD5 /* OCUnitIOSAppTestQueryRunner.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = OCUnitIOSAppTestQueryRunner.h; sourceTree = "<group>"; };
@@ -604,8 +600,6 @@
 				AAC1E0B01811F5C3005A4FD5 /* OCUnitTestQueryRunner.m */,
 				AAC1E0B31811F5E3005A4FD5 /* OCUnitOSXTestQueryRunner.h */,
 				AAC1E0B41811F5E3005A4FD5 /* OCUnitOSXTestQueryRunner.m */,
-				AAC1E0B71811F5F0005A4FD5 /* OCUnitIOSTestQueryRunner.h */,
-				AAC1E0B81811F5F0005A4FD5 /* OCUnitIOSTestQueryRunner.m */,
 				AAC1E0BB18121071005A4FD5 /* OCUnitIOSLogicTestQueryRunner.h */,
 				AAC1E0BC18121071005A4FD5 /* OCUnitIOSLogicTestQueryRunner.m */,
 				AAC1E0BF18121AC6005A4FD5 /* OCUnitIOSAppTestQueryRunner.h */,
@@ -741,7 +735,6 @@
 				EEB31CEA17C685E500CFB0E1 /* OCEventState.m in Sources */,
 				EEB31CF117C6A1EF00CFB0E1 /* OCTestEventState.m in Sources */,
 				AAC1E0BD18121071005A4FD5 /* OCUnitIOSLogicTestQueryRunner.m in Sources */,
-				AAC1E0B91811F5F0005A4FD5 /* OCUnitIOSTestQueryRunner.m in Sources */,
 				EEB31CF917C6D57B00CFB0E1 /* OCTestSuiteEventState.m in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
@@ -778,7 +771,6 @@
 				28AC9E3016DB3478009E5B9A /* OptionsTests.m in Sources */,
 				EE37291217E2886200554867 /* Reporter.m in Sources */,
 				283479A616E1B242003C3B77 /* RunTestsActionTests.m in Sources */,
-				AAC1E0BA1811F5F0005A4FD5 /* OCUnitIOSTestQueryRunner.m in Sources */,
 				2869F3C617C82FB80078F078 /* TestableExecutionInfo.m in Sources */,
 				283479AE16E2A736003C3B77 /* BuildTestsAction.m in Sources */,
 				AAC1E0B21811F5C3005A4FD5 /* OCUnitTestQueryRunner.m in Sources */,

xctool/xctool.xcodeproj/xcshareddata/xcschemes/xctool.xcscheme
@@ -126,6 +126,20 @@
             buildForAnalyzing = "YES">
             <BuildableReference
                BuildableIdentifier = "primary"
+               BlueprintIdentifier = "28AF3212182716E20051F658"
+               BuildableName = "sim-shim.dylib"
+               BlueprintName = "sim-shim"
+               ReferencedContainer = "container:../sim-shim/sim-shim.xcodeproj">
+            </BuildableReference>
+         </BuildActionEntry>
+         <BuildActionEntry
+            buildForTesting = "YES"
+            buildForRunning = "YES"
+            buildForProfiling = "YES"
+            buildForArchiving = "YES"
+            buildForAnalyzing = "YES">
+            <BuildableReference
+               BuildableIdentifier = "primary"
                BlueprintIdentifier = "283CCA4116C2EA3700F2E343"
                BuildableName = "xctool"
                BlueprintName = "xctool"

xctool/xctool/OCUnitIOSAppTestQueryRunner.h
@@ -14,8 +14,8 @@
 // limitations under the License.
 //
 
-#import "OCUnitIOSTestQueryRunner.h"
+#import "OCUnitTestQueryRunner.h"
 
-@interface OCUnitIOSAppTestQueryRunner : OCUnitIOSTestQueryRunner
+@interface OCUnitIOSAppTestQueryRunner : OCUnitTestQueryRunner
 
 @end

xctool/xctool/OCUnitIOSAppTestQueryRunner.m
@@ -23,20 +23,23 @@
 
 - (NSTask *)createTaskForQuery
 {
-  NSTask *task;
-  task = CreateTaskInSameProcessGroupWithArch([self cpuType]);
-  NSString *bundlePath = [self bundlePath];
-  NSString *testHostPath = [self testHostPath];
+  NSString *version = [_buildSettings[@"SDK_NAME"] stringByReplacingOccurrencesOfString:@"iphonesimulator" withString:@""];
 
-  [task setLaunchPath:testHostPath];
-  [task setArguments:@[ bundlePath ]];
-
-  [task setEnvironment:[self envForQueryInIOSBundleWithAdditionalEnv:@{
-    // Inserted this dylib, which will then load whatever is in `OtestQueryBundlePath`.
-    @"DYLD_INSERT_LIBRARIES" : [XCToolLibPath() stringByAppendingPathComponent:@"otest-query-ios-dylib.dylib"],
-    // The test bundle that we want to query from.
-    @"OtestQueryBundlePath" : bundlePath,
-  }]];
+  NSTask *task = CreateTaskInSameProcessGroup();
+  [task setLaunchPath:[XcodeDeveloperDirPath() stringByAppendingPathComponent:@"Platforms/iPhoneSimulator.platform/usr/bin/sim"]];
+  [task setArguments:@[[@"--arch=" stringByAppendingString:([self cpuType] == CPU_TYPE_X86_64) ? @"64" : @"32"],
+                       [@"--sdk=" stringByAppendingString:version],
+                       @"--environment=merge",
+                       [self testHostPath],
+                       ]];
+  [task setEnvironment:@{// We insert a shim into the 'sim' process so that we can
+                         // add some extra environment variables to otest-query-ios as it's launched.
+                         @"DYLD_INSERT_LIBRARIES" : [XCToolLibPath() stringByAppendingPathComponent:@"sim-shim.dylib"],
+                         // sim-sim will launched the spawned process with this value for DYLD_INSERT_LIBRARIES.
+                         @"SIMSHIM_DYLD_INSERT_LIBRARIES" : [XCToolLibPath() stringByAppendingPathComponent:@"otest-query-ios-dylib.dylib"],
+                         // The test bundle that we want to query from, as loaded by otest-query-ios-dylib.
+                         @"OtestQueryBundlePath" : [self bundlePath],
+                         }];
   return task;
 }
 

xctool/xctool/OCUnitIOSLogicTestQueryRunner.h
@@ -14,8 +14,8 @@
 // limitations under the License.
 //
 
-#import "OCUnitIOSTestQueryRunner.h"
+#import "OCUnitTestQueryRunner.h"
 
-@interface OCUnitIOSLogicTestQueryRunner : OCUnitIOSTestQueryRunner
+@interface OCUnitIOSLogicTestQueryRunner : OCUnitTestQueryRunner
 
 @end

xctool/xctool/OCUnitIOSLogicTestQueryRunner.m
@@ -23,10 +23,16 @@
 
 - (NSTask *)createTaskForQuery
 {
-  NSTask *task = CreateTaskInSameProcessGroupWithArch([self cpuType]);
-  [task setLaunchPath:[XCToolLibExecPath() stringByAppendingPathComponent:@"otest-query-ios"]];
-  [task setEnvironment:[self envForQueryInIOSBundleWithAdditionalEnv:nil]];
-  [task setArguments:@[ [self bundlePath] ]];
+  NSString *version = [_buildSettings[@"SDK_NAME"] stringByReplacingOccurrencesOfString:@"iphonesimulator" withString:@""];
+
+  NSTask *task = CreateTaskInSameProcessGroup();
+  [task setLaunchPath:[XcodeDeveloperDirPath() stringByAppendingPathComponent:@"Platforms/iPhoneSimulator.platform/usr/bin/sim"]];
+  [task setArguments:@[[@"--arch=" stringByAppendingString:([self cpuType] == CPU_TYPE_X86_64) ? @"64" : @"32"],
+                       [@"--sdk=" stringByAppendingString:version],
+                       @"--environment=merge",
+                       [XCToolLibExecPath() stringByAppendingPathComponent:@"otest-query-ios"],
+                       [self bundlePath],
+                       ]];
   return task;
 }
 

xctool/xctool/OCUnitIOSTestQueryRunner.m
@@ -1,71 +1,0 @@
-//
-// Copyright 2013 Facebook
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
-#import "OCUnitIOSTestQueryRunner.h"
-
-#import "iPhoneSimulatorRemoteClient.h"
-#import "TaskUtil.h"
-#import "XCToolUtil.h"
-
-NSString *SimulatorSDKRootPathWithVersion(NSString *version)
-{
-  return [NSString stringWithFormat:@"%@/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator%@.sdk",
-          XcodeDeveloperDirPath(),
-          version];
-}
-
-@implementation OCUnitIOSTestQueryRunner
-
-- (NSString *)sdk
-{
-  NSString *sdk = _buildSettings[@"SDK_NAME"];
-  NSCAssert([sdk hasPrefix:@"iphonesimulator"], @"Only iphonesimulator SDKs are supported.");
-  return sdk;
-}
-
-- (NSDictionary *)envForQueryInIOSBundleWithAdditionalEnv:(NSDictionary *)additionalEnv
-{
-  NSString *version = [[self sdk] stringByReplacingOccurrencesOfString:@"iphonesimulator" withString:@""];
-  NSString *productVersion = GetProductVersionForSDKVersion(version);
-  NSString *simulatorHome = [NSString stringWithFormat:@"%@/Library/Application Support/iPhone Simulator/%@", NSHomeDirectory(), productVersion];
-  NSString *sdkRootPath = SimulatorSDKRootPathWithVersion(version);
-
-  NSMutableDictionary *env = [NSMutableDictionary dictionaryWithDictionary:@{
-    @"CFFIXED_USER_HOME" : simulatorHome,
-    @"HOME" : simulatorHome,
-    @"IPHONE_SHARED_RESOURCES_DIRECTORY" : simulatorHome,
-    @"DYLD_ROOT_PATH" : sdkRootPath,
-    @"IPHONE_SIMULATOR_ROOT" : sdkRootPath,
-    @"NSUnbufferedIO" : @"YES"
-  }];
-  if (additionalEnv) {
-    [env addEntriesFromDictionary:additionalEnv];
-  }
-
-  return env;
-}
-
-// We need to default to 32-bit for all iOS tests, but shouldn't mess with it
-// if it's been explicitly specified already.
-- (cpu_type_t)cpuType
-{
-  if ([super cpuType] == CPU_TYPE_ANY) {
-    [super setCpuType:CPU_TYPE_I386];
-  }
-  return [super cpuType];
-}
-
-@end

xctool/xctool/RunTestsAction.m
@@ -21,7 +21,6 @@
 #import "OCUnitIOSAppTestRunner.h"
 #import "OCUnitIOSDeviceTestRunner.h"
 #import "OCUnitIOSLogicTestRunner.h"
-#import "OCUnitIOSTestQueryRunner.h"
 #import "OCUnitOSXAppTestRunner.h"
 #import "OCUnitOSXLogicTestRunner.h"
 #import "OCUnitOSXTestQueryRunner.h"

xctool/xctool-tests/LaunchHandlers.m
@@ -158,8 +158,15 @@
 
     BOOL isOtestQuery = NO;
 
-    if ([[task launchPath] hasSuffix:@"otest-query-osx"] ||
-        [[task launchPath] hasSuffix:@"otest-query-ios"]) {
+    if ([[task launchPath] hasSuffix:@"usr/bin/sim"]) {
+      // iOS tests get queried through the 'sim' launcher.
+      for (NSString *arg in [task arguments]) {
+        if ([arg hasSuffix:@"otest-query-ios"]) {
+          isOtestQuery = YES;
+          break;
+        }
+      }
+    } else if ([[task launchPath] hasSuffix:@"otest-query-osx"]) {
       isOtestQuery = YES;
     }
 