fc25095cb3bab4128231afc2a171b9fa234a21f4
Fred Potter <fpotter@fb.com>
2013-11-13 11:02:55 -0800

    reducing some code duplication in OCUnitTestRunnerTests
    Also, our tests are going to need a reference to the OCUnitTestRunner in
    a coming change.

--- xctool/xctool-tests/OCUnitTestRunnerTests.m
+++ xctool/xctool-tests/OCUnitTestRunnerTests.m
@@ -50,25 +50,21 @@ static OCUnitTestRunner *TestRunner(Class cls, NSDictionary *settings)
 
 #pragma mark iOS Tests
 
-- (void)simulateIOSApplicationTestWithSettings:(NSDictionary*)testSettings
-                                     putConfig:(DTiPhoneSimulatorSessionConfig**)configPtr
-                                      putError:(NSString**)errPtr
+- (void)runTestsForRunner:(OCUnitTestRunner *)runner
+           andReturnError:(NSString **)error
+            sessionConfig:(DTiPhoneSimulatorSessionConfig **)sessionConfig
 {
-  *configPtr = nil;
-  *errPtr = nil;
-
   [Swizzler whileSwizzlingSelector:@selector(launchAndWaitForExit)
                forInstancesOfClass:[SimulatorLauncher class]
                          withBlock:
    ^(SimulatorLauncher *self, SEL sel) {
      // Pretend it launched and succeeded, but save the config so we can check it.
-     *configPtr = [[self->_session sessionConfig] retain];
+     *sessionConfig = [[self->_session sessionConfig] retain];
      return YES;
    }
                           runBlock:
    ^{
-     OCUnitTestRunner *runner = TestRunner([OCUnitIOSAppTestRunner class], testSettings);
-     [runner runTestsWithError:errPtr];
+     [runner runTestsWithError:error];
    }];
 }
 
@@ -85,7 +81,11 @@ static OCUnitTestRunner *TestRunner(Class cls, NSDictionary *settings)
   DTiPhoneSimulatorSessionConfig *config;
   NSString *err;
 
-  [self simulateIOSApplicationTestWithSettings:testSettings putConfig:&config putError:&err];
+  OCUnitTestRunner *runner = TestRunner([OCUnitIOSAppTestRunner class], testSettings);
+
+  [self runTestsForRunner:runner
+           andReturnError:&err
+            sessionConfig:&config];
 
   assertThat(config, notNilValue());
   assertThat([config simulatedApplicationLaunchArgs],
@@ -111,7 +111,11 @@ static OCUnitTestRunner *TestRunner(Class cls, NSDictionary *settings)
   DTiPhoneSimulatorSessionConfig *config;
   NSString *err;
 
-  [self simulateIOSApplicationTestWithSettings:testSettings putConfig:&config putError:&err];
+  OCUnitTestRunner *runner = TestRunner([OCUnitIOSAppTestRunner class], testSettings);
+
+  [self runTestsForRunner:runner
+           andReturnError:&err
+            sessionConfig:&config];
 
   assertThat(config, nilValue());
   assertThat(err, containsString(@"TEST_HOST"));
@@ -127,38 +131,33 @@ static OCUnitTestRunner *TestRunner(Class cls, NSDictionary *settings)
                                                        error:nil]);
   NSDictionary *testSettings = allSettings[@"TestProject-LibraryTests"];
 
-  [[FakeTaskManager sharedManager] runBlockWithFakeTasks:^{
-    OCUnitTestRunner *runner = TestRunner([OCUnitIOSLogicTestRunner class], testSettings);
+  NSArray *launchedTasks;
+  NSString *error;
 
-    NSString *error = nil;
-    [runner runTestsWithError:&error];
+  OCUnitTestRunner *runner = TestRunner([OCUnitIOSLogicTestRunner class], testSettings);
+  [self runTestsForRunner:runner
+           andReturnError:&error
+                    tasks:&launchedTasks];
 
-    NSArray *launchedTasks = [[FakeTaskManager sharedManager] launchedTasks];
-    assertThatInteger([launchedTasks count], equalToInteger(1));
+  assertThatInteger([launchedTasks count], equalToInteger(1));
 
-    assertThat([launchedTasks[0] arguments],
-               containsArray(@[@"-SomeArg",
-                               @"SomeVal",
-                               ]));
-    assertThat([launchedTasks[0] environment][@"SIMSHIM_SomeEnvKey"],
-               equalTo(@"SomeEnvValue"));
-  }];
+  assertThat([launchedTasks[0] arguments],
+             containsArray(@[@"-SomeArg",
+                             @"SomeVal",
+                             ]));
+  assertThat([launchedTasks[0] environment][@"SIMSHIM_SomeEnvKey"],
+             equalTo(@"SomeEnvValue"));
 }
 
 #pragma mark OSX Tests
 
-- (void)simulateOSXApplicationTestWithSettings:(NSDictionary*)testSettings
-                                      putTasks:(NSArray**)tasksptr
-                                      putError:(NSString**)errPtr
+- (void)runTestsForRunner:(OCUnitTestRunner *)runner
+           andReturnError:(NSString **)error
+                    tasks:(NSArray **)launchedTasks
 {
-  *tasksptr = nil;
-  *errPtr = nil;
-
   [[FakeTaskManager sharedManager] runBlockWithFakeTasks:^{
-    OCUnitTestRunner *runner = TestRunner([OCUnitOSXAppTestRunner class], testSettings);
-    [runner runTestsWithError:errPtr];
-
-    *tasksptr = [[[FakeTaskManager sharedManager] launchedTasks] retain];
+    [runner runTestsWithError:error];
+    *launchedTasks = [[[FakeTaskManager sharedManager] launchedTasks] retain];
   }];
 }
 
@@ -175,7 +174,10 @@ static OCUnitTestRunner *TestRunner(Class cls, NSDictionary *settings)
   NSArray *launchedTasks;
   NSString *err;
 
-  [self simulateOSXApplicationTestWithSettings:testSettings putTasks:&launchedTasks putError:&err];
+  OCUnitTestRunner *runner = TestRunner([OCUnitOSXAppTestRunner class], testSettings);
+  [self runTestsForRunner:runner
+           andReturnError:&err
+                    tasks:&launchedTasks];
 
   assertThatInteger([launchedTasks count], equalToInteger(1));
   assertThat([launchedTasks[0] arguments],
@@ -199,7 +201,10 @@ static OCUnitTestRunner *TestRunner(Class cls, NSDictionary *settings)
   NSArray *launchedTasks;
   NSString *err;
 
-  [self simulateOSXApplicationTestWithSettings:testSettings putTasks:&launchedTasks putError:&err];
+  OCUnitTestRunner *runner = TestRunner([OCUnitOSXAppTestRunner class], testSettings);
+  [self runTestsForRunner:runner
+           andReturnError:&err
+                    tasks:&launchedTasks];
 
   assertThatInteger([launchedTasks count], equalToInteger(0));
   assertThat(err, containsString(@"TEST_HOST"));
@@ -214,22 +219,22 @@ static OCUnitTestRunner *TestRunner(Class cls, NSDictionary *settings)
                                                        error:nil]);
   NSDictionary *testSettings = allSettings[@"TestProject-Library-OSXTests"];
 
-  [[FakeTaskManager sharedManager] runBlockWithFakeTasks:^{
-    OCUnitTestRunner *runner = TestRunner([OCUnitOSXLogicTestRunner class], testSettings);
+  NSString *error = nil;
+  NSArray *launchedTasks = nil;
 
-    NSString *error = nil;
-    [runner runTestsWithError:&error];
+  OCUnitTestRunner *runner = TestRunner([OCUnitOSXLogicTestRunner class], testSettings);
+  [self runTestsForRunner:runner
+           andReturnError:&error
+                    tasks:&launchedTasks];
 
-    NSArray *launchedTasks = [[FakeTaskManager sharedManager] launchedTasks];
-    assertThatInteger([launchedTasks count], equalToInteger(1));
+  assertThatInteger([launchedTasks count], equalToInteger(1));
 
-    assertThat([launchedTasks[0] arguments],
-               containsArray(@[@"-SomeArg",
-                               @"SomeVal",
-                               ]));
-    assertThat([launchedTasks[0] environment][@"SomeEnvKey"],
-               equalTo(@"SomeEnvValue"));
-  }];
+  assertThat([launchedTasks[0] arguments],
+             containsArray(@[@"-SomeArg",
+                             @"SomeVal",
+                             ]));
+  assertThat([launchedTasks[0] environment][@"SomeEnvKey"],
+             equalTo(@"SomeEnvValue"));
 }
 
 - (void)testTestArgumentsAlwaysIncludesCommonItems
