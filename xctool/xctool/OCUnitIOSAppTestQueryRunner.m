4d5fda6f7ac8f84fa0c3abce63628e28e685c9a3
Fred Potter <fpotter@fb.com>
2013-11-13 19:01:02 -0800

    otest-query-ios-dylib -> otest-query-lib-ios
    I like this name better.  We're about to add an OS X version.

--- xctool/xctool/OCUnitIOSAppTestQueryRunner.m
+++ xctool/xctool/OCUnitIOSAppTestQueryRunner.m
@@ -29,8 +29,8 @@
                                           version,
                                           [self testHostPath],
                                           @[],
-                                          @{@"DYLD_INSERT_LIBRARIES" : [XCToolLibPath() stringByAppendingPathComponent:@"otest-query-ios-dylib.dylib"],
-                                            // The test bundle that we want to query from, as loaded by otest-query-ios-dylib.
+                                          @{@"DYLD_INSERT_LIBRARIES" : [XCToolLibPath() stringByAppendingPathComponent:@"otest-query-lib-ios.dylib"],
+                                            // The test bundle that we want to query from, as loaded by otest-query-lib-ios.dylib.
                                             @"OtestQueryBundlePath" : [self bundlePath],
                                             });
 }
