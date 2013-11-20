88fd50c62e11c6779d47e9b62d09b64eec246030
Felix Schulze <fschulze@autoscout24.com>
2013-11-20 13:32:10 +0100

    Add archivePath option to ArchiveAction

--- xctool/xctool/ArchiveAction.m
+++ xctool/xctool/ArchiveAction.m
@@ -4,6 +4,12 @@
 #import "Options.h"
 #import "XCToolUtil.h"
 
+@interface ArchiveAction ()
+
+@property (nonatomic, retain) NSString *archivePath;
+
+@end
+
 @implementation ArchiveAction
 
 + (NSString *)name
@@ -17,10 +23,37 @@
                         arrayByAddingObjectsFromArray:[options commonXcodeBuildArgumentsForSchemeAction:@"ArchiveAction"
                                                                                        xcodeSubjectInfo:xcodeSubjectInfo]]
                         arrayByAddingObject:@"archive"];
+  if (_archivePath)
+  {
+    arguments = [arguments arrayByAddingObjectsFromArray:@[@"-archivePath", _archivePath]];
+  }
   return RunXcodebuildAndFeedEventsToReporters(arguments,
                                                @"archive",
                                                [options scheme],
                                                [options reporters]);
 }
+
++ (NSArray *)options
+{
+  return
+  @[
+    [Action actionOptionWithName:@"archivePath"
+                         aliases:nil
+                     description:@"PATH where created archive will be placed."
+                       paramName:@"PATH"
+                           mapTo:@selector(setArchivePath:)],
+    ];
+}
+
+- (void)dealloc {
+  self.archivePath = nil;
+  [super dealloc];
+}
+
+- (void)setArchivePath:(NSString *)archivePath
+{
+  _archivePath = archivePath;
+}
+
 
 @end
