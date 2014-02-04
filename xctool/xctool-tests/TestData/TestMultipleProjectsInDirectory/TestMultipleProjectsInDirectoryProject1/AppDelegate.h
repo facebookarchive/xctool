1d3575eeb73e0945bc43d5a2cc69092bab1fd3bb
Rollin Su <rollinsu@gmail.com>
2014-02-04 15:18:34 -0800

    Search for project to build if project/workspace are not specified
    -Rename unit test and error message (Specifying either -project or -workspace is no longer required)
    -add description method for Options for easier debugging

---
+++ xctool/xctool-tests/TestData/TestMultipleProjectsInDirectory/TestMultipleProjectsInDirectoryProject1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestMultipleProjectsInDirectoryProject1
+//
+//  Created by Rollin Su on 2/3/14.
+//  Copyright (c) 2014 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ xctool/xctool-tests/TestData/TestMultipleProjectsInDirectory/TestMultipleProjectsInDirectoryProject1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestMultipleProjectsInDirectoryProject1
+//
+//  Created by Rollin Su on 2/3/14.
+//  Copyright (c) 2014 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
