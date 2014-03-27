e287085bee00d8046695fd408a0a92e022d02293
Meinhard Gredig <github@flexcoding.de>
2014-03-27 16:03:08 +0100

    Fix for missing @class directives in SimulatorHost classes

--- xctool/SimulatorHost/ISHSDKInfo.h
+++ xctool/SimulatorHost/ISHSDKInfo.h
@@ -4,6 +4,7 @@
 //     class-dump is Copyright (C) 1997-1998, 2000-2001, 2004-2013 by Steve Nygard.
 //
 
+@class NSDictionary, NSString;
 @interface ISHSDKInfo : NSObject
 {
     BOOL _usesLegacyPathInfoStruct;
