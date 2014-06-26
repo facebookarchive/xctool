2b56bdeae1c1377e5f227307d5908e955274b4a7
Aleksey Kononov <nekto@fb.com>
2014-06-25 18:39:42 -0700

    Xcode 6 implementation of SimulatorInfo and headers cleaning

--- xctool/Headers/iPhoneSimulatorRemoteClient/DTiPhoneSimulatorRemoteClient.h
+++ xctool/Headers/iPhoneSimulatorRemoteClient/DTiPhoneSimulatorRemoteClient.h
@@ -35,25 +35,19 @@ typedef void DVTTask;
 typedef void DVTConfinementServiceConnection;
 
 @interface DVTiPhoneSimulatorMessenger : NSObject
-{
-  DTiPhoneSimulatorSession *_session;
-  id _readyMessageHandler;
-  id _runningMessageHandler;
-  id _appDidLaunchMessageHandler;
-  id _appDidQuitMessageHandler;
-  id _appPIDExitedMessageHandler;
-  id _toolDidLaunchMessageHandler;
-}
+
+@property(copy, nonatomic) id toolDidLaunchMessageHandler;
+@property(copy, nonatomic) id appPIDExitedMessageHandler;
+@property(copy, nonatomic) id appDidQuitMessageHandler;
+@property(copy, nonatomic) id appDidLaunchMessageHandler;
+@property(copy, nonatomic) id runningMessageHandler;
+@property(copy, nonatomic) id readyMessageHandler;
+@property(readonly) DTiPhoneSimulatorSession *session;
 
 + (id)messengerForSession:(id)arg1 withConnection:(id)arg2;
 + (id)messengerForSession:(id)arg1;
-@property(copy, nonatomic) id toolDidLaunchMessageHandler; // @synthesize toolDidLaunchMessageHandler=_toolDidLaunchMessageHandler;
-@property(copy, nonatomic) id appPIDExitedMessageHandler; // @synthesize appPIDExitedMessageHandler=_appPIDExitedMessageHandler;
-@property(copy, nonatomic) id appDidQuitMessageHandler; // @synthesize appDidQuitMessageHandler=_appDidQuitMessageHandler;
-@property(copy, nonatomic) id appDidLaunchMessageHandler; // @synthesize appDidLaunchMessageHandler=_appDidLaunchMessageHandler;
-@property(copy, nonatomic) id runningMessageHandler; // @synthesize runningMessageHandler=_runningMessageHandler;
-@property(copy, nonatomic) id readyMessageHandler; // @synthesize readyMessageHandler=_readyMessageHandler;
-@property(readonly) DTiPhoneSimulatorSession *session; // @synthesize session=_session;
+
+- (id)initWithSession:(id)arg1;
 - (void)doUbiquityFetchEvent;
 - (void)doFetchEventForPID:(int)arg1;
 - (void)backgroundAllApps:(int)arg1;
@@ -66,7 +60,6 @@ typedef void DVTConfinementServiceConnection;
 - (void)waitPID:(int)arg1 withAppPIDExitedMessagedHandler:(id)arg2;
 - (void)disconnectFromService;
 - (BOOL)connectToServiceWithSessionOnLaunch:(BOOL)arg1 simulatorPID:(int *)arg2 error:(id *)arg3;
-- (id)initWithSession:(id)arg1;
 
 @end
 
@@ -112,10 +105,9 @@ typedef void DVTConfinementServiceConnection;
   NSMutableDictionary *_awaitingSemaphores;
   NSMutableDictionary *_awaitingResponses;
   NSMutableSet *_waitingAppPIDs;
-  DVTConfinementServiceConnection *_connection;
 }
 
-@property(readonly) DVTConfinementServiceConnection *connection; // @synthesize connection=_connection;
+@property(readonly) DVTConfinementServiceConnection *connection;
 - (void)handleNotificationResponse:(id)arg1;
 - (void)waitPID:(int)arg1 withAppPIDExitedMessagedHandler:(id)arg2;
 - (void)startSimulatorToolSessionWithParameters:(id)arg1;
@@ -136,27 +128,17 @@ typedef void DVTConfinementServiceConnection;
 @end
 
 @interface DTiPhoneSimulatorSession : NSObject
-{
-  int _simulatedApplicationPID;
-  int _simulatorPID;
-  NSString *_uuid;
-  id <DTiPhoneSimulatorSessionDelegate> _delegate;
-  NSString *_simulatedAppPath;
-  long long _sessionLifecycleProgress;
-  NSTimer *_timeoutTimer;
-  DTiPhoneSimulatorSessionConfig *_sessionConfig;
-  DVTiPhoneSimulatorMessenger *_messenger;
-}
 
-@property(retain) DVTiPhoneSimulatorMessenger *messenger; // @synthesize messenger=_messenger;
-@property(copy, nonatomic) DTiPhoneSimulatorSessionConfig *sessionConfig; // @synthesize sessionConfig=_sessionConfig;
-@property(retain, nonatomic) NSTimer *timeoutTimer; // @synthesize timeoutTimer=_timeoutTimer;
-@property(nonatomic) long long sessionLifecycleProgress; // @synthesize sessionLifecycleProgress=_sessionLifecycleProgress;
-@property int simulatorPID; // @synthesize simulatorPID=_simulatorPID;
-@property(copy) NSString *simulatedAppPath; // @synthesize simulatedAppPath=_simulatedAppPath;
-@property int simulatedApplicationPID; // @synthesize simulatedApplicationPID=_simulatedApplicationPID;
-@property(retain, nonatomic) id <DTiPhoneSimulatorSessionDelegate> delegate; // @synthesize delegate=_delegate;
-@property(copy, nonatomic) NSString *uuid; // @synthesize uuid=_uuid;
+@property(retain) DVTiPhoneSimulatorMessenger *messenger;
+@property(copy, nonatomic) DTiPhoneSimulatorSessionConfig *sessionConfig;
+@property(retain, nonatomic) NSTimer *timeoutTimer;
+@property(nonatomic) long long sessionLifecycleProgress;
+@property int simulatorPID;
+@property(copy) NSString *simulatedAppPath;
+@property int simulatedApplicationPID;
+@property(retain, nonatomic) id <DTiPhoneSimulatorSessionDelegate> delegate;
+@property(copy, nonatomic) NSString *uuid;
+
 - (void)doUbiquityFetchEvent;
 - (void)doFetchEventForPID:(int)arg1;
 - (void)backgroundAllApps:(int)arg1;
@@ -180,9 +162,6 @@ typedef void DVTConfinementServiceConnection;
 - (void)requestEndWithTimeout:(double)arg1;
 - (BOOL)requestStartWithConfig:(id)arg1 timeout:(double)arg2 error:(id *)arg3;
 - (BOOL)_setUpSimulatorMessengerWithConfig:(id)arg1 error:(id *)arg2;
-- (id)description;
-- (void)dealloc;
-- (id)init;
 
 @end
 
@@ -210,68 +189,59 @@ typedef void DVTConfinementServiceConnection;
 }
 
 + (id)displayNameForDeviceFamily:(id)arg1;
-@property(retain) id confinementService; // @synthesize confinementService=_confinementService;
-@property(retain) NSFileHandle *stderrFileHandle; // @synthesize stderrFileHandle=_stderrFileHandle;
-@property(retain) NSFileHandle *stdoutFileHandle; // @synthesize stdoutFileHandle=_stdoutFileHandle;
-@property(retain) NSFileHandle *stdinFileHandle; // @synthesize stdinFileHandle=_stdinFileHandle;
-@property(copy) NSString *simulatedApplicationStdErrPath; // @synthesize simulatedApplicationStdErrPath=_simulatedApplicationStdErrPath;
-@property(copy) NSString *simulatedApplicationStdOutPath; // @synthesize simulatedApplicationStdOutPath=_simulatedApplicationStdOutPath;
-@property BOOL simulatedApplicationShouldWaitForDebugger; // @synthesize simulatedApplicationShouldWaitForDebugger=_simulatedApplicationShouldWaitForDebugger;
-@property(copy) NSDictionary *simulatedApplicationLaunchEnvironment; // @synthesize simulatedApplicationLaunchEnvironment=_simulatedApplicationLaunchEnvironment;
-@property(copy) NSArray *simulatedApplicationLaunchArgs; // @synthesize simulatedApplicationLaunchArgs=_simulatedApplicationLaunchArgs;
-@property(copy) NSNumber *pid; // @synthesize pid=_pid;
-@property(copy) DTiPhoneSimulatorApplicationSpecifier *applicationToSimulateOnStart; // @synthesize applicationToSimulateOnStart=_applicationToSimulateOnStart;
-@property(copy) NSNumber *simulatedDisplayScale; // @synthesize simulatedDisplayScale=_simulatedDisplayScale;
-@property(copy) NSNumber *simulatedDisplayHeight; // @synthesize simulatedDisplayHeight=_simulatedDisplayHeight;
-@property(copy) NSString *simulatedArchitecture; // @synthesize simulatedArchitecture=_simulatedArchitecture;
-@property(copy) NSNumber *simulatedDeviceFamily; // @synthesize simulatedDeviceFamily=_simulatedDeviceFamily;
-@property(retain) NSString *simulatedDeviceInfoName; // @synthesize simulatedDeviceInfoName=_simulatedDeviceInfoName;
-@property(copy) DTiPhoneSimulatorSystemRoot *simulatedSystemRoot; // @synthesize simulatedSystemRoot=_simulatedSystemRoot;
-@property(copy) NSString *localizedClientName; // @synthesize localizedClientName=_localizedClientName;
-@property BOOL launchForBackgroundFetch; // @synthesize launchForBackgroundFetch=_launchForBackgroundFetch;
-- (id)description;
-- (id)copyWithZone:(struct _NSZone *)arg1;
-- (id)init;
+
+@property(retain) id confinementService;
+@property(retain) NSFileHandle *stderrFileHandle;
+@property(retain) NSFileHandle *stdoutFileHandle;
+@property(retain) NSFileHandle *stdinFileHandle;
+@property(copy) NSString *simulatedApplicationStdErrPath;
+@property(copy) NSString *simulatedApplicationStdOutPath;
+@property BOOL simulatedApplicationShouldWaitForDebugger;
+@property(copy) NSDictionary *simulatedApplicationLaunchEnvironment;
+@property(copy) NSArray *simulatedApplicationLaunchArgs;
+@property(copy) NSNumber *pid;
+@property(copy) DTiPhoneSimulatorApplicationSpecifier *applicationToSimulateOnStart;
+@property(copy) NSNumber *simulatedDisplayScale;
+@property(copy) NSNumber *simulatedDisplayHeight;
+@property(copy) NSString *simulatedArchitecture;
+@property(copy) NSNumber *simulatedDeviceFamily;
+@property(retain) NSString *simulatedDeviceInfoName;
+@property(copy) DTiPhoneSimulatorSystemRoot *simulatedSystemRoot;
+@property(copy) NSString *localizedClientName;
+@property BOOL launchForBackgroundFetch;
 
 @end
 
 @interface DTiPhoneSimulatorSystemRoot : NSObject <NSCopying>
-{
-  NSString *sdkRootPath;
-  NSString *sdkVersion;
-  NSString *sdkDisplayName;
-}
 
+#if XCODE_VERSION >= 0600
+@property(readonly) id runtime;
+#endif
+@property(copy) NSString *sdkDisplayName;
+@property(copy) NSString *sdkVersion;
+@property(copy) NSString *sdkRootPath;
+
+#if XCODE_VERSION >= 0600
++ (id)rootWithSimRuntime:(id)arg1;
+#endif
 + (id)rootWithSDKVersion:(id)arg1;
 + (id)rootWithSDKPath:(id)arg1;
 + (id)defaultRoot;
 + (id)knownRoots;
-+ (void)initialize;
-@property(copy) NSString *sdkDisplayName; // @synthesize sdkDisplayName;
-@property(copy) NSString *sdkVersion; // @synthesize sdkVersion;
-@property(copy) NSString *sdkRootPath; // @synthesize sdkRootPath;
-- (id)description;
+
 - (long long)compare:(id)arg1;
-- (id)copyWithZone:(struct _NSZone *)arg1;
-- (BOOL)isEqual:(id)arg1;
 - (id)initWithSDKPath:(id)arg1;
 
 @end
 
 @interface DTiPhoneSimulatorApplicationSpecifier : NSObject <NSCopying>
-{
-  NSString *appPath;
-  NSString *bundleID;
-  NSString *toolPath;
-}
+
+@property(copy, nonatomic) NSString *toolPath;
+@property(copy, nonatomic) NSString *bundleID;
+@property(copy, nonatomic) NSString *appPath;
 
 + (id)specifierWithToolPath:(id)arg1;
 + (id)specifierWithApplicationBundleIdentifier:(id)arg1;
 + (id)specifierWithApplicationPath:(id)arg1;
-@property(copy, nonatomic) NSString *toolPath; // @synthesize toolPath;
-@property(copy, nonatomic) NSString *bundleID; // @synthesize bundleID;
-@property(copy, nonatomic) NSString *appPath; // @synthesize appPath;
-- (id)description;
-- (id)copyWithZone:(struct _NSZone *)arg1;
 
 @end
--- xctool/xctool/SimulatorWrapper/SimulatorInfoXcode6.m
+++ xctool/xctool/SimulatorWrapper/SimulatorInfoXcode6.m
@@ -16,6 +16,272 @@
 
 #import "SimulatorInfoXcode6.h"
 
+#import "DTiPhoneSimulatorRemoteClient.h"
+#import "SimDevice.h"
+#import "SimDeviceSet.h"
+#import "SimDeviceType.h"
+#import "SimRuntime.h"
+#import "XcodeBuildSettings.h"
+#import "XCToolUtil.h"
+
+static const NSInteger KProductTypeIphone = 1;
+static const NSInteger KProductTypeIpad = 2;
+
+@interface SimRuntime (Latest)
++ (SimRuntime *)latest;
+@end
+
+@implementation SimRuntime (Latest)
+
++ (SimRuntime *)latest
+{
+  NSArray *sorted = [[SimRuntime supportedRuntimes] sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"version" ascending:YES]]];
+  return [sorted lastObject];
+}
+
+@end
+
 @implementation SimulatorInfoXcode6
+@synthesize buildSettings = _buildSettings;
+@synthesize cpuType = _cpuType;
+@synthesize deviceName = _deviceName;
+@synthesize OSVersion = _OSVersion;
+
+- (NSNumber *)simulatedDeviceFamily
+{
+  return @([_buildSettings[Xcode_TARGETED_DEVICE_FAMILY] integerValue]);
+}
+
+- (NSString *)simulatedDeviceInfoName
+{
+  if (_deviceName) {
+    return _deviceName;
+  }
+
+  NSString *probableDeviceName;
+  switch ([[self simulatedDeviceFamily] integerValue]) {
+    case KProductTypeIphone:
+      probableDeviceName = @"iPhone";
+      break;
+
+    case KProductTypeIpad:
+      probableDeviceName = @"iPad";
+      break;
+  }
+
+  DTiPhoneSimulatorSystemRoot *systemRoot = [DTiPhoneSimulatorSystemRoot rootWithSDKVersion:[self sdkVersion]];
+  if (!systemRoot) {
+    return probableDeviceName;
+  }
+
+  // return lowest device that supports simulated sdk
+  SimRuntime *runtime = systemRoot.runtime;
+  NSMutableArray *supportedDeviceTypes = [NSMutableArray array];
+  for (SimDeviceType *deviceType in [SimDeviceType supportedDeviceTypes]) {
+    if ([runtime supportsDeviceType:deviceType]) {
+      [supportedDeviceTypes addObject:deviceType];
+    }
+  }
+
+  NSAssert([supportedDeviceTypes count] > 0, @"There are no available devices that support provided sdk: %@. Supported devices: %@", [self sdkVersion], [[SimDeviceType supportedDevices] valueForKeyPath:@"name"]);
+  return [supportedDeviceTypes[0] name];
+}
+
+- (NSString *)simulatedArchitecture
+{
+  switch (self.cpuType) {
+    case CPU_TYPE_I386:
+      return @"i386";
+
+    case CPU_TYPE_X86_64:
+      return @"x86_64";
+  }
+  return @"i386";
+}
+
+- (NSString *)sdkVersion
+{
+  NSString *sdkVersion = [_buildSettings[Xcode_IPHONEOS_DEPLOYMENT_TARGET] stringByReplacingOccurrencesOfString:@"iphonesimulator" withString:@""];
+  if (self.OSVersion) {
+    if ([self.OSVersion isEqualTo:@"latest"]) {
+      sdkVersion = [[SimRuntime latest] versionString];
+    } else {
+      sdkVersion = self.OSVersion;
+    }
+  }
+  return sdkVersion;
+}
+
+- (NSString *)maxSdkVersionForSimulatedDevice
+{
+  NSMutableArray *runtimes = [[[[self class] _runtimesSupportedByDevice:[self simulatedDeviceInfoName]] mutableCopy] autorelease];
+  [runtimes sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"version" ascending:YES]]];
+  return [[runtimes lastObject] versionString];
+}
+
+- (NSString *)simulatedSdkVersion
+{
+  if (self.OSVersion) {
+    return [self sdkVersion];
+  } else {
+    return [self maxSdkVersionForSimulatedDevice];
+  }
+}
+
+- (NSString *)simulatedSdkRootPath
+{
+  return [[self systemRootForSimulatedSdk] sdkRootPath];
+}
+
+- (NSString *)simulatedSdkShortVersion
+{
+  return [[[self systemRootForSimulatedSdk] runtime] versionString];
+}
+
+- (DTiPhoneSimulatorSystemRoot *)systemRootForSimulatedSdk
+{
+  NSString *sdkVersion = [self simulatedSdkVersion];
+  DTiPhoneSimulatorSystemRoot *systemRoot = [DTiPhoneSimulatorSystemRoot rootWithSDKVersion:sdkVersion];
+  if (systemRoot) {
+    return systemRoot;
+  }
+
+  SimRuntime *runtime = [[self class] _runtimeForSdkVersion:sdkVersion];
+  systemRoot = [DTiPhoneSimulatorSystemRoot rootWithSimRuntime:runtime];
+  NSAssert(runtime != nil, @"Unable to instantiate DTiPhoneSimulatorSystemRoot for sdk version: %@. Available roots: %@", sdkVersion, [DTiPhoneSimulatorSystemRoot knownRoots]);
+  return systemRoot;
+}
+
+- (NSDictionary *)simulatorLaunchEnvironment
+{
+  // Sometimes the TEST_HOST will be wrapped in double quotes.
+  NSString *testHostPath = [_buildSettings[Xcode_TEST_HOST] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"\""]];
+
+  NSString *ideBundleInjectionLibPath = @"/../../Library/PrivateFrameworks/IDEBundleInjection.framework/IDEBundleInjection";
+  NSString *testBundlePath = [NSString stringWithFormat:@"%@/%@", _buildSettings[Xcode_BUILT_PRODUCTS_DIR], _buildSettings[Xcode_FULL_PRODUCT_NAME]];
+
+  return @{
+           @"DYLD_FALLBACK_FRAMEWORK_PATH" : [[self simulatedSdkRootPath] stringByAppendingPathComponent:@"/Developer/Library/Frameworks"],
+           @"DYLD_FRAMEWORK_PATH" : _buildSettings[Xcode_TARGET_BUILD_DIR],
+           @"DYLD_LIBRARY_PATH" : _buildSettings[Xcode_TARGET_BUILD_DIR],
+           @"DYLD_INSERT_LIBRARIES" : [@[
+                                         [XCToolLibPath() stringByAppendingPathComponent:@"otest-shim-ios.dylib"],
+                                         ideBundleInjectionLibPath,
+                                         ] componentsJoinedByString:@":"],
+           @"NSUnbufferedIO" : @"YES",
+           @"XCInjectBundle" : testBundlePath,
+           @"XCInjectBundleInto" : testHostPath,
+           };
+}
+
+#pragma mark -
+#pragma mark v6 methods
+
+- (SimRuntime *)simulatedRuntime
+{
+  return [[self systemRootForSimulatedSdk] runtime];
+}
+
+- (SimDevice *)simulatedDevice
+{
+  SimRuntime *runtime = [self simulatedRuntime];
+  SimDeviceType *deviceType = [SimDeviceType supportedDeviceTypesByAlias][[self simulatedDeviceInfoName]];
+  for (SimDevice *device in [[SimDeviceSet defaultSet] availableDevices]) {
+    if ([device.deviceType isEqual:deviceType] &&
+        [device.runtime isEqual:runtime]) {
+      return device;
+    }
+  }
+  return nil;
+}
+
+#pragma mark -
+#pragma mark Class Methods
+
++ (NSArray *)availableDevices
+{
+  return [[SimDeviceType supportedDeviceTypesByName] allKeys];
+}
+
++ (BOOL)isDeviceAvailableWithAlias:(NSString *)deviceName
+{
+  return [[SimDeviceType supportedDeviceTypesByAlias] objectForKey:deviceName] != nil;
+}
+
++ (NSString *)deviceNameForAlias:(NSString *)deviceAlias
+{
+  SimDeviceType *deviceType = [[SimDeviceType supportedDeviceTypesByAlias] objectForKey:deviceAlias];
+  return [deviceType name];
+}
+
++ (BOOL)isSdkVersion:(NSString *)sdkVersion supportedByDevice:(NSString *)deviceName
+{
+  SimDeviceType *deviceType = [SimDeviceType supportedDeviceTypesByAlias][deviceName];
+  SimRuntime *runtime = [self _runtimeForSdkVersion:sdkVersion];
+
+  return [runtime supportsDeviceType:deviceType];
+}
+
++ (NSString *)sdkVersionForOSVersion:(NSString *)osVersion
+{
+  if ([osVersion isEqualToString:@"latest"]) {
+    return [[SimRuntime latest] versionString];
+  } else {
+    return [[self _runtimeForSdkVersion:osVersion] versionString];
+  }
+}
+
++ (NSArray *)availableSdkVersions
+{
+  return [[SimRuntime supportedRuntimes] valueForKeyPath:@"versionString"];
+}
+
++ (NSArray *)sdksSupportedByDevice:(NSString *)deviceName
+{
+  NSArray *runtimes = [self _runtimesSupportedByDevice:deviceName];
+  return [runtimes valueForKeyPath:@"versionString"];
+}
+
++ (cpu_type_t)cpuTypeForDevice:(NSString *)deviceName
+{
+  SimDeviceType *deviceType = [SimDeviceType supportedDeviceTypesByAlias][deviceName];
+  if ([deviceType.supportedArchs containsObject:@(CPU_TYPE_X86_64)]) {
+    return CPU_TYPE_X86_64;
+  } else {
+    return CPU_TYPE_I386;
+  }
+}
+
+#pragma mark -
+#pragma mark - Helpers
+
++ (NSArray *)_runtimesSupportedByDevice:(NSString *)deviceName
+{
+  NSMutableArray *supportedRuntimes = [NSMutableArray array];
+  SimDeviceType *deviceType = [SimDeviceType supportedDeviceTypesByAlias][deviceName];
+  for (SimRuntime *runtime in [SimRuntime supportedRuntimes]) {
+    if ([runtime supportsDeviceType:deviceType]) {
+      [supportedRuntimes addObject:runtime];
+    }
+  }
+  return supportedRuntimes;
+}
+
++ (SimRuntime *)_runtimeForSdkVersion:(NSString *)sdkVersion
+{
+  NSArray *runtimes = [SimRuntime supportedRuntimes];
+  for (SimRuntime *runtime in runtimes) {
+    if ([runtime.versionString hasPrefix:sdkVersion]) {
+      return runtime;
+    }
+  }
+  return nil;
+}
+
++ (SimRuntime *)_runtimeForSDKPath:(NSString *)sdkPath
+{
+  DTiPhoneSimulatorSystemRoot *root = [DTiPhoneSimulatorSystemRoot rootWithSDKPath:sdkPath];
+  return [root runtime];
+}
 
 @end
