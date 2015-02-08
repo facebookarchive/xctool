a124d149b044a2989a0cdbd36ddbab389de31523
Jerome Morissard <morissardj@gmail.com>
2015-02-08 19:24:53 +0100

    add archivePath management

--- xctool/xctool/Options.h
+++ xctool/xctool/Options.h
@@ -46,9 +46,11 @@
 @property (nonatomic, copy) NSString *findProjectPath;
 @property (nonatomic, copy) NSString *resultBundlePath;
 @property (nonatomic, copy) NSArray *findTargetExcludePaths;
+@property (nonatomic, copy) NSString *archivePath;
 
 @property (nonatomic, assign) BOOL showBuildSettings;
 @property (nonatomic, assign) BOOL showTasks;
+@property (nonatomic, assign) BOOL archive;
 
 @property (nonatomic, retain) NSMutableDictionary *buildSettings;
 @property (nonatomic, retain) NSMutableDictionary *userDefaults;
@@ -99,4 +101,5 @@
 - (BOOL)validateAndReturnXcodeSubjectInfo:(XcodeSubjectInfo **)xcodeSubjectInfo
                              errorMessage:(NSString **)errorMessage;
 
+- (void)addAction:(Action *)oneAction;
 @end
