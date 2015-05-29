bec1b7e436198cf77d2dd39246a83d4bbcbf7e55
Ben Hamilton <beng@fb.com>
2015-05-29 15:55:44 -0700

    Explicitly install java7 and ant

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
+
 # Build xctool with xcodebuild
 scripts/build.sh && ./xctool.sh -workspace xctool.xcworkspace -scheme xctool build build-tests run-tests
 
