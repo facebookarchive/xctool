05ad14769402733192139f6758b3fc9c21ec444f
Justin Spahr-Summers <jspahrsummers@fb.com>
2015-10-01 10:44:13 +0100

    Add OTestQuery test for custom XCTest invocations

---
+++ xctool/xctool-tests/TestData/TestProject-Library-XCTest-Custom/TestProject-Library-XCTest-Custom/TestProject_Library_XCTest_Custom.m
@@ -0,0 +1,13 @@
+//
+//  TestProject_Library_XCTest_Custom.m
+//  TestProject-Library-XCTest-Custom
+//
+//  Created by Justin Spahr-Summers on 2015-10-01.
+//  Copyright © 2015 Facebook. All rights reserved.
+//
+
+#import "TestProject_Library_XCTest_Custom.h"
+
+@implementation TestProject_Library_XCTest_Custom
+
+@end
