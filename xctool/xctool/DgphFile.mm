7ce202df02b0f20f558b741e9b84a3e332e451b5
Yiding Jia <yiding@fb.com>
2015-11-19 15:01:34 -0800

    dgph1.00 support for xcode 6

--- xctool/xctool/DgphFile.mm
+++ xctool/xctool/DgphFile.mm
@@ -91,33 +91,81 @@ void pVarLenPrefixedList_(std::ifstream &stream, F func) {
   }
 }
 
-} // anonymous namespace
+DgphFile parseDgph104(std::ifstream &input) {
+  pVarLenPrefixedString_(input); // build date
+  pVarLenPrefixedString_(input); // build time
 
-DgphFile DgphFile::loadFromFile(const char *path) {
-  std::ifstream input(path);
-  input.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-
-  try {
-    std::string magicversion = pTake(input, 8);
-    if (magicversion != "DGPH1.04") { // DGPH1.04 is used for xcode 7.
-      NSLog(@"Unsupported version of DGPH file: %s", magicversion.c_str());
-      return DgphFile();
+  pVarLenPrefixedList_(input, [](std::ifstream &input) {
+    int isVirtual = input.get();
+    if (!isVirtual) {
+      pVarLenIntLE(input); // parent node id
     }
-    pVarLenPrefixedString_(input); // build date ignored
-    pVarLenPrefixedString_(input); // build time ignored
+    pVarLenPrefixedString_(input); // node name
+  });
 
-    pVarLenPrefixedList_(input, [](std::ifstream &input) {
-      int isVirtual = input.get();
-      if (!isVirtual) {
-        pVarLenIntLE(input); // parent node id
-      }
-      pVarLenPrefixedString_(input); // node name
-    });
+  pVarLenIntLE(input); // fsroot node id
+  pVarLenIntLE(input); // projectroot node id
 
-    pVarLenIntLE(input); // fsroot node id
-    pVarLenIntLE(input); // projectroot node id
+  // node states ignored
+  pVarLenPrefixedList_(input, [](std::ifstream &input) {
+    pVarLenIntLE(input); // node id
+    pVarLenIntLE(input); // options
+    uint64_t err = pVarLenIntLE(input); // err
+    if (!err) {
+      pVarLenIntLE(input); // mtime
+      pVarLenIntLE(input); // size
+      pVarLenIntLE(input); // mode
+    }
+  });
 
-    // node states ignored
+  auto invocations = pVarLenPrefixedList(input, [](std::ifstream &input) -> std::vector<std::string> {
+    pVarLenPrefixedString_(input); // identifier
+    input.ignore(16); // signature hash
+    pVarLenPrefixedString_(input); // desc
+    auto args = pVarLenPrefixedList(input, pVarLenPrefixedString);
+    pVarLenPrefixedList_(input, pVarLenPrefixedString_); // env
+    pVarLenIntLE(input); // working dir node id
+    input.ignore(8); // start time double
+    input.ignore(8); // end time double
+    pVarLenIntLE(input); // exitStatus
+    pVarLenPrefixedString_(input); // builder uuid
+    pVarLenPrefixedString_(input); // activity log (SLF0 encoded)
+    pVarLenPrefixedList_(input, pVarLenIntLE); // input node ids
+    pVarLenPrefixedList_(input, pVarLenIntLE); // output node ids
+    return std::move(args);
+  });
+
+  return DgphFile(std::move(invocations));
+}
+
+DgphFile parseDgph100(std::ifstream &input) {
+  pVarLenPrefixedString_(input); // build date
+  pVarLenPrefixedString_(input); // build time
+
+  pVarLenPrefixedList_(input, [](std::ifstream &input) {
+    int isVirtual = input.get();
+    if (!isVirtual) {
+      pVarLenIntLE(input); // parent node id
+    }
+    pVarLenPrefixedString_(input); // node name
+  });
+
+  pVarLenIntLE(input); // fsroot node id
+  pVarLenIntLE(input); // projectroot node id
+
+  auto invocations = pVarLenPrefixedList(input, [](std::ifstream &input) -> std::vector<std::string> {
+    pVarLenPrefixedString_(input); // identifier
+    input.ignore(16); // signature hash
+    pVarLenPrefixedString_(input); // desc
+    auto args = pVarLenPrefixedList(input, pVarLenPrefixedString);
+    pVarLenPrefixedList_(input, pVarLenPrefixedString_); // env
+    pVarLenIntLE(input); // working dir node id
+    input.ignore(8); // start time double
+    input.ignore(8); // end time double
+    pVarLenIntLE(input); // exitStatus
+    pVarLenPrefixedString_(input); // builder uuid
+    pVarLenPrefixedString_(input); // activity log (SLF0 encoded)
+    // input node states
     pVarLenPrefixedList_(input, [](std::ifstream &input) {
       pVarLenIntLE(input); // node id
       pVarLenIntLE(input); // options
@@ -128,29 +176,34 @@ DgphFile DgphFile::loadFromFile(const char *path) {
         pVarLenIntLE(input); // mode
       }
     });
+    return std::move(args);
+  });
 
-    auto invocations = pVarLenPrefixedList(input, [](std::ifstream &input) -> std::vector<std::string> {
-      pVarLenPrefixedString_(input); // identifier
-      input.ignore(16); // signature hash
-      pVarLenPrefixedString_(input); // desc
-      auto args = pVarLenPrefixedList(input, pVarLenPrefixedString);
-      pVarLenPrefixedList_(input, pVarLenPrefixedString_); // env
-      pVarLenIntLE(input); // working dir node id
-      input.ignore(8); // start time double
-      input.ignore(8); // end time double
-      pVarLenIntLE(input); // exitStatus
-      pVarLenPrefixedString_(input); // builder uuid
-      pVarLenPrefixedString_(input); // activity log (SLF0 encoded)
-      pVarLenPrefixedList_(input, pVarLenIntLE); // input node ids
-      pVarLenPrefixedList_(input, pVarLenIntLE); // output node ids
-      return std::move(args);
-    });
+  return DgphFile(std::move(invocations));
+}
 
-    return DgphFile(std::move(invocations));
+} // anonymous namespace
+
+DgphFile DgphFile::loadFromFile(const char *path) {
+  std::ifstream input(path);
+  input.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+  try {
+    std::string magicversion = pTake(input, 8);
+    if (magicversion == "DGPH1.04") {  // Used for xcode 7
+      return parseDgph104(input);
+    } else if (magicversion == "DGPH1.00") {  // Used for xcode 6
+      return parseDgph100(input);
+    } else if (magicversion.find("DGPH") == 0) {
+      NSLog(@"Unsupported version of DGPH file: %s, %s", magicversion.c_str(), path);
+      return DgphFile();
+    } else {
+      NSLog(@"input is not a DGPH file: %s", path);
+    }
   } catch (const std::exception &e) {
-    NSLog(@"DGPH failed to load: %s", e.what());
+    NSLog(@"DGPH failed to load: %s, %s", e.what(), path);
     return DgphFile();
   }
-  NSLog(@"DGPH failed to load");
+  NSLog(@"DGPH failed to load: %s", path);
   return DgphFile();
 }
