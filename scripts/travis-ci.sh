23a5c39abcab0469a3224551faa6a361d80b50e4
Aleksey Kononov <nekto@whatsapp.com>
2019-12-10 18:27:00 +0300

    Make Travis CI output more readable.

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -e
-set -x
-
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
 
 OUTPUT_DIR=$(mktemp -d -t xctool-release)
 BUILD_OUTPUT_DIR="$OUTPUT_DIR"/build
 RELEASE_OUTPUT_DIR="$OUTPUT_DIR"/release
 XCTOOL_DIR=$(cd $(dirname $0)/..; pwd)
+
+[[ -n "${TRAVIS}" ]] && echo "travis_fold:start:build_xctool_tests"
+[[ -n "${TRAVIS}" ]] && echo "Build xctool and tests"
 
 # Build xctool with xcodebuild
 xcodebuild \
@@ -21,6 +21,8 @@ xcodebuild \
   -IDECustomBuildProductsPath="$BUILD_OUTPUT_DIR/Products" \
   -IDECustomBuildIntermediatesPath="$BUILD_OUTPUT_DIR/Intermediates" \
   XT_INSTALL_ROOT="$RELEASE_OUTPUT_DIR"
+
+[[ -n "${TRAVIS}" ]] && echo "travis_fold:end:build_xctool_tests"
 
 if [[ ! -x "$RELEASE_OUTPUT_DIR"/bin/xctool ]]; then
   echo "ERROR: xctool binary is missing."
